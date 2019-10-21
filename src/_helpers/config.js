e00a2e826781bb352044163810ffd833470119c1
Abhishek Sharma <abhishek.s@mdxblocks.com>
2019-10-21 23:17:02 +0530

    wrong api url fixed

--- src/_helpers/config.js
+++ src/_helpers/config.js
@@ -1,6 +1,6 @@
 export const config = {
     apiUrl: window.location.hostname === "geonet.localtunnel.me"
             ? 'https://geonet-server.localtunnel.me/api'
-            : 'http://474eb3cd.ngrok.io/api',
+            : 'http://localhost:3009/api',
     env: "live" // live || local
 };
