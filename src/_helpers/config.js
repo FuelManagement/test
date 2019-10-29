ace7780ed4336abcbcf989b4d4b0c184ea55a66a
Abhishek Sharma <abhishek.s@mdxblocks.com>
2019-10-30 01:56:01 +0530

    bug fix

--- src/_helpers/config.js
+++ src/_helpers/config.js
@@ -1,6 +1,6 @@
 export const config = {
     apiUrl: window.location.hostname === "geonet.localtunnel.me"
             ? 'https://geonet-server.localtunnel.me/api'
-            : window.location.hostname+':3009/api',
+            : 'http://'+window.location.hostname+':3009/api',
     env: "live" // live || local
 };
