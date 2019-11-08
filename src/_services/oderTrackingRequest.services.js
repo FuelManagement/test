da824f2a25d9459021334d993b86418b8fba3cc5
Krishna <krishna.bayann@gmail.com>
2019-11-08 12:15:17 +0530

    merge conflicsts resolved

---
+++ src/_services/oderTrackingRequest.services.js
@@ -0,0 +1,17 @@
+import { authHeader, config, Utils,Common_JsonData } from '../_helpers';
+const { handleError, handleResponse } = Utils;
+import $ from 'jquery';
+const OrderReqDetails=Common_JsonData.orederTrackingRequestDetails;
+export const OrderTrackingService= {
+    getOTRDetailsBysupplier
+}
+function getOTRDetailsBysupplier() {
+    console.log('req',OrderReqDetails.data);
+    return OrderReqDetails;
+    // let user = JSON.parse(localStorage.getItem('user'));
+    // const requestOptions = {
+    //     method: 'GET',
+    //     headers: authHeader()
+    // };
+    //return data;//fetch(config.apiUrl + '/ordertracking/getOTRDetailsBysupplier', requestOptions).then(handleResponse)
+}
