97a2140ebc3d08d8df01d4c378ac672d8196a468
Krishna <krishna.bayann@gmail.com>
2019-11-09 23:13:33 +0530

    order tracking services

--- src/_helpers/table_helper.js
+++ src/_helpers/table_helper.js
@@ -1037,12 +1037,12 @@ let Table_Config = {
 			 {
 				Header: 'Status',
 				accessor: 'status',
-				Cell: row => <span style={{ display: 'block', textAlign: 'center',fontWeight: 'bold'  ,color:
+				Cell: row => <span style={{ display: 'block', textAlign: 'left',fontWeight: 'bold'  ,color:
 				row.original.status === "Approve" ?'#29AD01': 'red'  }} title={row.original.status}>{row.original.status}</span>
 			}, {
 				Header: 'Date',
 				accessor: 'date',
-				Cell: row => <span style={{ display: 'block', textAlign: 'center' }} title={row.original.date}>{row.original.date}</span>
+				Cell: row => <span style={{ display: 'block', textAlign: 'left' }} title={row.original.date}>{row.original.date}</span>
 			}]
 		}
 	},
